b6b9082d3a9ff21a489b02eb743c683e9b0da66d
Documenter.jl <documenter@juliadocs.github.io>
2021-11-01 21:32:43 +0000

    build based on a9b8de3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ElectronGas","category":"page"},{"location":"#ElectronGas","page":"Home","title":"ElectronGas","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ElectronGas.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ElectronGas]","category":"page"}]
+}
