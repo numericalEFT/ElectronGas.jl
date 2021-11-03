02db8cc4d2006dbfa2a7772ce6fdb88f18eb2f46
Xiansheng Cai <iintsjds@gmail.com>
2021-11-03 16:42:01 -0400

    use standalone Polarization0

--- src/interaction.jl
+++ src/interaction.jl
@@ -38,67 +38,62 @@ A2=(C2-C1)/4.0/e0^2*π ;
 B1=6*A1/(D+1.0);
 B2=2*A2/(1.0-D);
 
-
-function RPA(q, n, β=β)
-    g = e0^2
-    kernal = 0.0
+function Polarization0_ZeroTemp(q, n, β=β)
     Π = 0.0
-    Π_r= 0.0
-    if abs(q) > EPS 
-        x = q/2/kF
-        ω_n = 2*π*n/β
-        y = me*ω_n/q/kF
-        
-        
-        if n == 0
-            if abs(q - 2*kF) > EPS
-                Π = me*kF/2/π^2*(1 + (1 -x^2)*log1p(4*x/((1-x)^2))/4/x)
-            else
-                Π = me*kF/2/π^2
-            end
-            kernal = - Π/( q^2/4/π/g  + Π )                 
+    x = q/2/kF
+    ω_n = 2*π*n/β
+    y = me*ω_n/q/kF
+
+    if n == 0
+        if abs(q - 2*kF) > EPS
+            Π = me*kF/2/π^2*(1 + (1 -x^2)*log1p(4*x/((1-x)^2))/4/x)
         else
-            if abs(q - 2*kF) > EPS
-               
-                if y^2 < 1e-4/EPS                    
-                    theta = atan( 2*y/(y^2+x^2-1) )
-                    if theta < 0
-                        theta = theta + π
-                    end
-                    @assert theta >= 0 && theta<= π
-                    Π = me*kF/2/π^2 * (1 + (1 -x^2 + y^2)*log1p(4*x/((1-x)^2+y^2))/4/x - y*theta)
-                else
-                    Π = me*kF/2/π^2 * (2.0/3.0/y^2  - 2.0/5.0/y^4) #+ (6.0 - 14.0*(ω_n/4.0)^2)/21.0/y^6)
-                end
-            else
-                theta = atan( 2/y )
+            Π = me*kF/2/π^2
+        end
+    else
+        if abs(q - 2*kF) > EPS
+            if y^2 < 1e-4/EPS                    
+                theta = atan( 2*y/(y^2+x^2-1) )
                 if theta < 0
                     theta = theta + π
                 end
                 @assert theta >= 0 && theta<= π
-                Π = me*kF/2/π^2*(1 + y^2*log1p(4/(y^2))/4 - y*theta)
+                Π = me*kF/2/π^2 * (1 + (1 -x^2 + y^2)*log1p(4*x/((1-x)^2+y^2))/4/x - y*theta)
+            else
+                Π = me*kF/2/π^2 * (2.0/3.0/y^2  - 2.0/5.0/y^4) #+ (6.0 - 14.0*(ω_n/4.0)^2)/21.0/y^6)
             end
-            #if( y^2 < 1e-6/EPS)
-            Π0 = Π / q^2
-            kernal = - Π0/( 1.0/4/π/g  + Π0 )
-            #else
-            #    kernal = - 2.0/( 3.0*π/4/g*ω_n^2  + 2.0 )
-            #end
-                           
-            #kernal = - Π/( (q^2+mass2)/4/π/g  + Π )
-
+        else
+            theta = atan( 2/y )
+            if theta < 0
+                theta = theta + π
+            end
+            @assert theta >= 0 && theta<= π
+            Π = me*kF/2/π^2*(1 + y^2*log1p(4/(y^2))/4 - y*theta)
         end
-       
-        #kernal = Π
-    else
-        kernal = 0
-        
     end
 
-    return kernal
+    return Π
 end
 
-function KO(q, n, β=β, type = :none)
+function RPA(q, n, β=β)
+    g = e0^2
+    kernel = 0.0
+    if abs(q) > EPS 
+        Π = Polarization0_ZeroTemp(q, n, β)
+        if n == 0
+            kernel = - Π/( q^2/4/π/g  + Π )
+        else
+            Π0 = Π / q^2
+            kernel = - Π0/( 1.0/4/π/g  + Π0 )
+        end
+    else
+        kernel = 0
+    end
+
+    return kernel
+end
+
+function KO(q, n, β=β)
     g = e0^2
     G_s=A1*q^2/(1.0+B1*q^2)+A2*q^2/(1.0+B2*q^2);
     G_a=A1*q^2/(1.0+B1*q^2)-A2*q^2/(1.0+B2*q^2);
@@ -106,49 +101,17 @@ function KO(q, n, β=β, type = :none)
     Ks, Ka = 0.0, 0.0
 
     if abs(q) > EPS 
-        x = q/2/kF
-        ω_n = 2*π*n/β
-        y = me*ω_n/q/kF
-        
-        
+        Π = Polarization0_ZeroTemp(q, n, β)
         if n == 0
-            if abs(q - 2*kF) > EPS
-                Π = me*kF/2/π^2*(1 + (1 -x^2)*log1p(4*x/((1-x)^2))/4/x)
-            else
-                Π = me*kF/2/π^2
-            end
-            kernal = - Π*(1-G_s)^2/( q^2/4/π/g  + Π*(1-G_s))-spin_factor*Π*(-G_a)^2/(q^2/4/π/g + Π*(-G_a))
+            Ks = - Π*(1-G_s)^2/( q^2/4/π/g  + Π*(1-G_s))
+            Ka = -Π*(-G_a)^2/(q^2/4/π/g + Π*(-G_a))
         else
-            if abs(q - 2*kF) > EPS
-                if y^2 < 1e-4/EPS                    
-                    theta = atan( 2*y/(y^2+x^2-1) )
-                    if theta < 0
-                        theta = theta + π
-                    end
-                    @assert theta >= 0 && theta<= π
-                    Π = me*kF/2/π^2 * (1 + (1 -x^2 + y^2)*log1p(4*x/((1-x)^2+y^2))/4/x - y*theta)
-                else
-                    Π = me*kF/2/π^2 * (2.0/3.0/y^2  - 2.0/5.0/y^4) #+ (6.0 - 14.0*(ω_n/4.0)^2)/21.0/y^6)
-                end
-            else
-                theta = atan( 2/y )
-                if theta < 0
-                    theta = theta + π
-                end
-                @assert theta >= 0 && theta<= π
-                Π = me*kF/2/π^2*(1 + y^2*log1p(4/y^2)/4 - y*theta)                       
-            end
             Π0 = Π / q^2
-            #kernal = - Π0/( 1.0/4/π/g  + Π0 )
             Ks = - Π0*(1-G_s)^2/( 1.0/4/π/g  + Π0*(1-G_s))
             Ka = -Π0*(-G_a)^2/(1.0/4/π/g + Π0*(-G_a))
-            #kernal = - Π/( (q^2+mass2)/4/π/g  + Π )
-
         end
-       
-        #kernal = Π
     else
-        
+        Ks, Ka = 0.0, 0.0
     end
 
     return Ks, Ka
