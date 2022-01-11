fe0e13e71a2ffc122136807ca98fc2221bdf3d8f
houpc <houpc96@gmail.com>
2022-01-11 20:11:30 +0800

    add spin-spin interaction

--- test/interaction.jl
+++ test/interaction.jl
@@ -10,8 +10,10 @@
         println(Interaction.KO(1.0, 1, param; pifunc=Interaction.Polarization.Polarization0_FiniteTemp))
 
         RPAs, RPAa = Interaction.RPAwrapped(100*param.EF,1e-8,[1e-8,0.5,1.0,2.0,10.0],param)
-        println(RPAs.dynamic, RPAs.insant)
-        println(RPAa.dynamic, RPAa.instant)
+        println(RPAs.dynamic)
+        println(RPAs.instant)
+        println(RPAa.dynamic)
+        println(RPAa.instant)
         KOs, KOa = Interaction.KOwrapped(100*param.EF,1e-8,[1e-8,0.5,1.0,2.0,10.0],param)
         println(KOs.dynamic)
         println(KOs.instant)
