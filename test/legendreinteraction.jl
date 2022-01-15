b63397f846d7c6f86ead53286c06b8f775c0b1f6
Xiansheng Cai <iintsjds@gmail.com>
2022-01-14 21:02:16 -0500

    test for DCKernel and sigma also seems ok

--- test/legendreinteraction.jl
+++ test/legendreinteraction.jl
@@ -31,11 +31,11 @@
 
         # test DCKernel
 
-        # kernel = LegendreInteraction.DCKernel0(param; spin_state=:sigma, Nk=8, rtol=1e-10)
-        # # kernel = LegendreInteraction.DCKernel(param, 100*param.EF, 1e-8, 5, 10*param.kF, 1e-7*param.kF, 4, :rpa,0,:sigma)
-        # # kernel2 = LegendreInteraction.DCKernel(kernel, 500.0)
-        # kF_label = searchsortedfirst(kernel.kgrid.grid, kernel.param.kF)
-        # qF_label = searchsortedfirst(kernel.qgrids[kF_label].grid, kernel.param.kF)
-        # println(kernel.kernel[kF_label,qF_label,:])
+        kernel = LegendreInteraction.DCKernel0(param; spin_state=:sigma, Nk=8, rtol=1e-10)
+        # kernel = LegendreInteraction.DCKernel(param, 100*param.EF, 1e-8, 5, 10*param.kF, 1e-7*param.kF, 4, :rpa,0,:sigma)
+        # kernel2 = LegendreInteraction.DCKernel(kernel, 500.0)
+        kF_label = searchsortedfirst(kernel.kgrid.grid, kernel.param.kF)
+        qF_label = searchsortedfirst(kernel.qgrids[kF_label].grid, kernel.param.kF)
+        println(kernel.kernel[kF_label,qF_label,:])
     end
 end
