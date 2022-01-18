d606e3f76d371bed9eb91f1d7c8eb093d9012080
Xiansheng Cai <iintsjds@gmail.com>
2022-01-18 14:10:57 -0500

    add more tests

--- test/legendreinteraction.jl
+++ test/legendreinteraction.jl
@@ -1,12 +1,14 @@
 @testset "LegendreInteraction" begin
 
-    @testset "DCKernel" begin
-        param = LegendreInteraction.Parameter.defaultUnit(1000.0, 1.0)
-        kF = param.kF
-        Nk, minK, order, maxK = 16, 1e-8, 6, 10.0
-        print(param)
+    # set parameters
+    param = LegendreInteraction.Parameter.defaultUnit(1000.0, 1.0)
+    kF = param.kF
+    Nk, minK, order, maxK = 16, 1e-8, 6, 10.0
+    print(param)
 
+    @testset "Helper function" begin
         # test helper function
+        # test with known result: bare coulomb interaction
         H1=LegendreInteraction.helper_function(
             1.0, 1, u->LegendreInteraction.interaction_instant(u,param,:sigma), param;
             Nk=Nk,minK=minK,order=order
@@ -15,7 +17,8 @@
             2.0, 1, u->LegendreInteraction.interaction_instant(u,param,:sigma), param;
             Nk=Nk,minK=minK,order=order
         )
-        println("$(H2-H1) == $(4*π*param.e0^2*log(2))")
+        # println("$(H2-H1) == $(4*π*param.e0^2*log(2))")
+        @test isapprox(H2-H1, 4*π*param.e0^2*log(2), rtol = 1e-4)
 
         helper_grid = CompositeGrid.LogDensedGrid(:cheb, [0.0, 2.1*maxK], [0.0, 2kF], 4, 0.001, 4)
         intgrid = CompositeGrid.LogDensedGrid(:gauss, [0.0, helper_grid[end]], [0.0,2kF], 16Nk, 0.01minK, 8order)
@@ -25,10 +28,17 @@
         for (yi, y) in enumerate(helper_grid)
             helper_old[yi] = LegendreInteraction.helper_function(y, 1, u->LegendreInteraction.interaction_instant(u,param,:sigma),param)
         end
-        println(helper .- helper[1])
-        println(helper_old .- helper_old[1])
-        println(helper_analytic .- helper_analytic[1])
+        # println(helper .- helper[1])
+        # println(helper_old .- helper_old[1])
+        # println(helper_analytic .- helper_analytic[1])
+        rtol = 1e-6
+        for (yi, y) in enumerate(helper_grid)
+            @test isapprox(helper[yi]-helper[1], helper_analytic[yi]-helper_analytic[1], rtol=rtol)
+            @test isapprox(helper_old[yi]-helper_old[1], helper_analytic[yi]-helper_analytic[1], rtol=rtol)
+        end
+    end
 
+    @testset "DCKernel" begin
         # test DCKernel
 
         kernel = LegendreInteraction.DCKernel0(param; spin_state=:sigma, Nk=8, rtol=1e-10)
