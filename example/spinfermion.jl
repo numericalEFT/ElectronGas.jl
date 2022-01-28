8434e4b70c164e4e64d3c93b7574c3f7ac2e2848
houpc <houpc96@gmail.com>
2022-01-28 11:11:52 +0800

    spinfermion example

--- example/spinfermion.jl
+++ example/spinfermion.jl
@@ -4,11 +4,11 @@ using ElectronGas
 using Test, Printf, DelimitedFiles
 
 dim = 2
-beta, rs = 1e2, 1.0
+beta, rs = 1e5, 1.0
 espin = 1.0
 param = Interaction.Parameter.defaultUnit(1 / beta, rs, dim)
 
-Λa = Polarization.Polarization0_ZeroTemp(0.0, 0, param) * (-espin^2) / param.ϵ0
+Λa = Polarization.Polarization0_ZeroTemp(0.0, 0, param) * param.spin * (-espin^2) / param.ϵ0
 println(Λa)
 
 param = Parameter.Para(param, e0 = 0.0, espin = espin, Λa = Λa)
@@ -31,7 +31,7 @@ println(kF_label)
 println(Σ.instant[1, 1, :])
 println(ΣR[1, 1, kF_label, :])
 
-# f = open("./data/Nk$Nk/SigmaIm_b1e2_m0.txt", "w")
+# f = open("./data/Nk$Nk/SigmaIm_b1e5_m0.txt", "w")
 for (n, sigma) in enumerate(ΣI[1, 1, kF_label, :])
     println(n, ' ', ωgrid.n[n], ' ', ωgrid.ωn[n], ' ', sigma)
     writedlm(f, [ωgrid.n[n] ωgrid.ωn[n] sigma])
