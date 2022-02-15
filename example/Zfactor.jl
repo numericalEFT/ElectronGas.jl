f6440edc88b349a162c163bb1079bf71775c982a
houpc <houpc96@gmail.com>
2022-02-15 08:55:38 +0800

    add some examples with MCIntegration

--- example/Zfactor.jl
+++ example/Zfactor.jl
@@ -1,44 +1,48 @@
-using Printf, LinearAlgebra, Distributed
+using Printf, LinearAlgebra
+using CompositeGrids
+using ElectronGas
+using Parameters, Random
+using MCIntegration
+using Lehmann
 
-const Ncpu = 16 # number of workers (CPU)
-const totalStep = 1e8
-addprocs(Ncpu)
+const steps = 1e7
 
-@everywhere using QuantumStatistics, Parameters, Random, LinearAlgebra
-@everywhere include("parameter.jl")
-@everywhere include("interaction.jl")
-@everywhere include("RPA.jl")
+include("parameter.jl")
+include("interaction.jl")
 
-@everywhere const kgrid = Grid.fermiK(kF, 3kF, 0.2kF, 16)  # external K grid for sigma
-@everywhere const dlr = DLR.DLRGrid(:fermi, 10EF, β, 1e-10)
+kgrid = CompositeGrid.LogDensedGrid(:uniform, [0.0, 3kF], [0.0, kF], 8, 0.01 * kF, 8) # external K grid for sigma
+# dlr = DLRGrid(Euv = 10EF, β = β, isFermi = true, rtol = 1e-10)
+const basic = Parameter.rydbergUnit(1 / beta, rs, dim)
 
-@everywhere struct Para{Q,T}
+struct Para{Q,T}
     dW0::Matrix{Float64}
     qgrid::Q
     τgrid::T # dedicated τgrid for dynamic interaction
     function Para()
-        qgrid = Grid.boseK(kF, 6kF, 0.1kF, 512) 
-        # τgrid = Grid.tau(β, EF / 5, 128) #for rs=4
-        τgrid = Grid.tau(β, EF / 25, 128) # for rs=1
-        # TODO: τgrid halflife works very strange
+        qgrid = CompositeGrid.LogDensedGrid(:uniform, [0.0, 6 * kF], [0.0, 2kF], 16, 0.01 * kF, 8)
+        # τgrid = CompositeGrid.LogDensedGrid(:uniform, [0.0, β], [0.0, β], 16, β * 1e-4, 8)
 
-        vqinv = [(q^2 + mass2) / (4π * e0^2) for q in qgrid.grid]
-        dW0 = dWRPA(vqinv, qgrid.grid, τgrid.grid, kF, β, spin, me) # dynamic part of the effective interaction
+        vqinv = [(q^2 + mass2) / (4π * e0^2) for q in qgrid.grid] # 3D Yukawa
+        # vqinv = [√(q^2 + mass2) / (2π * e0^2) for q in qgrid.grid] # 2D Yukawa
+        # dW0 = TwoPoint.dWRPA(vqinv, qgrid.grid, τgrid.grid, dim, EF, kF, β, spin, me) # dynamic part of the effective interaction
+        # dW0 = TwoPoint.dWKO(vqinv, qgrid.grid, τgrid.grid, dim, EF, kF, β, spin, me, fp, fm) # dynamic part of the effective interaction
+        Ws, Wa = Interaction.RPAwrapped(10EF, 1e-10, qgrid.grid, basic)
+        τgrid = Ws.dlrGrid.τ
+        dW0 = real.(matfreq2tau(Ws.dlrGrid, Ws.dynamic, τgrid, axis = 4))
+        println(dW0)
         return new{typeof(qgrid),typeof(τgrid)}(dW0, qgrid, τgrid)
     end
 end
 
-@everywhere function integrand(config)
+function integrand(config)
     if config.curr == 1
-        return 1.0
-    elseif config.curr == 2
         return eval2(config)
     else
         error("impossible!")
     end
 end
 
-@everywhere function eval2(config)
+function eval2(config)
     para = config.para
 
     K, T = config.var[1], config.var[2]
@@ -47,51 +51,50 @@ end
     kq = k + k0
     ω = (dot(kq, kq) - kF^2) / (2me)
     g = Spectral.kernelFermiT(τ, ω, β)
-    v, dW = interactionDynamic(config, k, 0.0, τ)
+    v, dW = interactionDynamic(para, k, 0.0, τ)
     phase = 1.0 / (2π)^3
     return g * dW * phase
 end
 
-@everywhere function measure(config)
+function measure(config)
     factor = 1.0 / config.reweight[config.curr]
     τ = config.var[2][1]
+    # println(config.observable[1][1])
     if config.curr == 1
-        config.observable[1][1] += factor
-    elseif config.curr == 2
         weight = integrand(config)
-        config.observable[2][1] += weight * sin(-π / β * τ) / abs(weight) * factor
-        config.observable[2][2] += weight * sin(π / β * τ) / abs(weight) * factor
+        config.observable[1] += weight * sin(π / β * τ) / abs(weight) * factor
+        config.observable[2] += weight * sin(3π / β * τ) / abs(weight) * factor
     else
         return
     end
 end
 
-@everywhere normalize(config) = config.observable[2] / config.observable[1][1]
-
 function fock(extn)
     para = Para()
     Ksize = length(kgrid.grid)
 
-    K = MonteCarlo.FermiK(dim, kF, 0.2 * kF, 10.0 * kF)
-    T = MonteCarlo.Tau(β, β / 2.0)
+    K = MCIntegration.FermiK(dim, kF, 0.2 * kF, 10.0 * kF)
+    T = MCIntegration.Tau(β, β / 2.0)
+    # Ext =
+    dof = [[1, 1],] # degrees of freedom of the Fock diagram
+    obs = zeros(2) # observable for the Fock diagram 
 
-    dof = ([0, 0], [1, 1]) # degrees of freedom of the normalization diagram and the bubble
-    obs = ([0.0, 0.0], [0.0, 0.0]) # observable for the normalization diagram and the bubble
+    config = MCIntegration.Configuration(steps, (K, T), dof, obs; para = para)
+    avg, std = MCIntegration.sample(config, integrand, measure; print = 0, Nblock = 16)
 
-    avg, std = MonteCarlo.sample(totalStep, (K, T), dof, obs, integrand, measure, normalize; para=para, print=10)
+    if isnothing(avg) == false
+        @printf("%10.6f   %10.6f ± %10.6f\n", -1.0, avg[1], std[1])
+        @printf("%10.6f   %10.6f ± %10.6f\n", 0.0, avg[2], std[2])
 
-
-    @printf("%10.6f   %10.6f ± %10.6f\n", -1.0, avg[1], std[1])
-    @printf("%10.6f   %10.6f ± %10.6f\n", 0.0, avg[2], std[2])
-
-    dS_dw = (avg[1] - avg[2]) / (2π / β)
-    error = (std[1] + std[2]) / (2π / β)
-    println("dΣ/diω= $dS_dw ± $error") 
-    Z = (1 / (1 + dS_dw))
-    Zerror = error / Z^2
-    println("Z=  $Z ± $Zerror")
-    # TODO: add errorbar estimation
-    # println
+        dS_dw = (avg[1] - avg[2]) / (2π / β)
+        error = (std[1] + std[2]) / (2π / β)
+        println("dΣ/diω= $dS_dw ± $error")
+        Z = (1 / (1 + dS_dw))
+        Zerror = error / Z^2
+        println("Z=  $Z ± $Zerror")
+        # TODO: add errorbar estimation
+        # println
+    end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
--- example/parameter.jl
+++ example/parameter.jl
@@ -7,16 +7,30 @@ const me = 0.5  # electron mass
 const dim = 3    # dimension (D=2 or 3, doesn't work for other D!!!)
 const spin = 2  # number of spins
 
-const rs = 3.0  
+const rs = 1.0
 const kF = (dim == 3) ? (9π / (2spin))^(1 / 3) / rs : sqrt(4 / spin) / rs
 const EF = kF^2 / (2me)
-const beta = 40
+const beta = 100
 const β = beta / EF
-const mass2 = 1
+const mass2 = 1e-6
+const NF = (dim == 3) ? spin * me * kF / 2 / π^2 : spin * me / 2 / π
+const qTF = sqrt(4π * e0^2 * NF)
+const fp = -0.0
+const fm = -0.0
 
-const Weight = SVector{2,Float64}
-const Base.abs(w::Weight) = abs(w[1]) + abs(w[2]) # define abs(Weight)
+# const Weight = SVector{2,Float64}
 const INL, OUTL, INR, OUTR = 1, 2, 3, 4
+const DI, EX = 1, 2
 # const Nf = (D==3) ? 
 
-println("rs=$rs, β=$β, kF=$kF, EF=$EF, mass2=$mass2")
+mutable struct Weight <: FieldVector{2,Float64}
+    d::Float64
+    e::Float64
+    Weight() = new(0.0, 0.0)
+    Weight(d, e) = new(d, e)
+end
+
+const Base.zero(::Type{Weight}) = Weight(0.0, 0.0)
+const Base.abs(w::Weight) = abs(w.d) + abs(w.e) # define abs(Weight)
+
+println("rs=$rs, β=$β, kF=$kF, EF=$EF, mass2=$mass2, NF=$NF, qTF/kF=$(qTF/kF)")
