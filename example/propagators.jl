866b66987b27828198539d5947fdddf94092980a
Xiansheng Cai <iintsjds@gmail.com>
2023-03-27 21:38:07 -0400

    wip

--- example/propagators.jl
+++ example/propagators.jl
@@ -8,6 +8,31 @@ using ElectronGas.Parameter
 using ElectronGas.GreenFunc
 using ElectronGas.CompositeGrids
 
+# shift tau to [0, β)
+function tau_fermi(t, β)
+    if 0 <= t < β
+        return t, 1.0
+    elseif β <= t < 2β
+        return t - β, -1.0
+    elseif -β <= t < 0
+        return t + β, -1.0
+    else
+        error("τ=$t out of range!")
+    end
+end
+
+function tau_bose(t, β)
+    if 0 <= t < β
+        return t, 1.0
+    elseif β <= t < 2β
+        return t - β, 1.0
+    elseif -β <= t < 0
+        return t + β, 1.0
+    else
+        error("τ=$t out of range!")
+    end
+end
+
 function rpa(param;
     mint=0.001,
     minK=0.001 * sqrt(param.T * param.me), maxK=10.0 * param.kF,
@@ -33,8 +58,17 @@ function interaction(k, prop; i=1)
 end
 
 function interaction(t, k, prop; i=1)
-    return Interp.interpND(view(prop.data, i, :, :), prop.mesh[2:3], (t, k))
+    t, factor = tau_fermi(t, prop.mesh[2].β)
+    return factor * Interp.interpND(view(prop.data, i, :, :), prop.mesh[2:3], (t, k))
     # return Interp.linear2D(view(prop.data, i, :, :), prop.mesh[2], prop.mesh[3], t, k)
+end
+
+function G0(t, k, param)
+    β = param.β
+    t, factor = tau_fermi(t, β)
+    ε = k^2 / 2 / param.me - param.μ
+    f = 1 / (exp(ε * β) + 1)
+    return -factor * exp(t * ε) * (1 - f)
 end
 
 end
@@ -45,23 +79,8 @@ if abspath(PROGRAM_FILE) == @__FILE__
     using .Propagators
 
     @testset "Propagators" begin
+
         param = Propagators.Parameter.defaultUnit(0.01, 1.0)
-
-        # mint = 0.001
-        # Nlog = floor(Int, 2.0 * log10(param.beta / mint))
-        # order = 6
-        # tgrid = Propagators.CompositeG.LogDensedGrid(:cheb, [0.0, param.β], [0.0, param.β], Nlog, mint, order)
-
-        # maxK = 10.0 * param.kF
-        # minK = 0.001 * sqrt(param.T * param.me)
-        # Nk = floor(Int, 2.0 * log10(maxK / minK))
-        # kgrid = Propagators.CompositeG.LogDensedGrid(:cheb, [0.0, maxK], [0.0, param.kF], Nk, minK, order)
-
-
-        # rpad, rpai = Propagators.RPAwrapped(100 * param.EF, 1e-10, kgrid, param)
-        # rpadlr = Propagators.to_dlr(rpad)
-
-        # rpat = Propagators.dlr_to_imtime(rpadlr, tgrid)
 
         rpai, rpat = Propagators.rpa(param)
 
@@ -73,9 +92,11 @@ if abspath(PROGRAM_FILE) == @__FILE__
         println(Propagators.interaction(p[2], rpai))
         @time Propagators.interaction(p..., rpat)
         @time Propagators.interaction(p[2], rpai)
+        @time Propagators.G0(p..., param)
 
         @time Propagators.interaction(p..., rpat)
         @time Propagators.interaction(p[2], rpai)
+        @time Propagators.G0(p..., param)
     end
 
 end
