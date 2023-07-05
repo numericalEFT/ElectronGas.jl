f0a1db9529d02751e1b53515c03f216daf4ccf47
Xiansheng Cai <iintsjds@gmail.com>
2023-07-05 18:27:08 -0400

    wip

--- example/pcf_resummed.jl
+++ example/pcf_resummed.jl
@@ -12,6 +12,37 @@ function load_AB(fname)
     return param, A, B
 end
 
+function extend_AB(A, B, param;
+    Nk=8, minterval=0.001, order=4)
+    # extend A and B to 0
+    oldwgrid = A.mesh[1]
+    Euv = oldwgrid[end]
+    wgrid = CompositeGrids.CompositeG.LogDensedGrid(:cheb, [0.0, Euv], [0.0, 0.1param.EF], Nk, minterval, order)
+    newA = GreenFunc.MeshArray(wgrid; dtype=Float64)
+    newB = GreenFunc.MeshArray(wgrid, wgrid; dtype=Float64)
+    for i in 1:length(wgrid)
+        w = wgrid[i]
+        if w < oldwgrid[1]
+            newA.data[i] = A.data[1]
+        else
+            newA.data[i] = CompositeGrids.Interp.interp1D(A.data, A.mesh[1], w)
+        end
+        for j in 1:length(wgrid)
+            v = wgrid[j]
+            if w < oldwgrid[1] && v < oldwgrid[1]
+                newB.data[i, j] = B.data[1, 1]
+            elseif w < oldwgrid[1]
+                newB.data[i, j] = CompositeGrids.Interp.interp1D(view(B.data, 1, :), B.mesh[2], v)
+            elseif v < oldwgrid[1]
+                newB.data[i, j] = CompositeGrids.Interp.interp1D(view(B.data, :, 1), B.mesh[1], w)
+            else
+                newB.data[i, j] = CompositeGrids.Interp.linear2D(B.data, B.mesh[1], B.mesh[2], w, v)
+            end
+        end
+    end
+    return newA, newB
+end
+
 function symmetrize_b(B)
     wgrid = B.mesh[1]
     sB = similar(B)
@@ -102,12 +133,15 @@ using Test
 @testset "pcf resummed" begin
     fname = "run/data/PCFresumdlr_3000010.jld2"
     param, A, B = load_AB(fname)
-    num = 9
+    A, B = extend_AB(A, B, param)
+    num = 11
     betas = [400 * 2^(i - 1) for i in 1:num]
-    for beta in betas
+    lamus = zeros(Float64, length(betas))
+    for i in 1:length(betas)
+        beta = betas[i]
         newA, newB = interp_AB(beta / param.EF, A, B, param)
         lamu, R = pcf_loop_ab(newA, newB, param)
         println("lamu=$lamu")
+        lamus[i] = lamu
     end
-
 end
